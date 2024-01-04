321343f57505702e66f25a37e295f88b29a35be9
wasan <wasanawwad2020@gmail.com>
2024-01-04 17:53:22 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Index from './component/users/index/Index'
+import Create from './component/users/create/Create'
+
+export default function App() {
+  return (
+   <Routes>
+    <Route path='/' />
+    <Route path='/users/create' element= {<Create />} />
+    <Route path='/users/index' element={<Index />} />
+
+    
+   </Routes>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
