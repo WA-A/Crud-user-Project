e4793624e8edd610321aa3e209a1b3c9dbafa9a0
wasan <wasanawwad2020@gmail.com>
2024-01-05 22:14:19 +0200

    comment

--- src/shared/Input.jsx
+++ src/shared/Input.jsx
@@ -8,6 +8,12 @@ function Input( {id,title,type,name,handlData,customClasses}) {
       <input type={type} name={name} className={`form-control ${customClasses}`} 
     id={id}  onChange={handlData}  />
      {errors[name]&& <p className='text-danger'>{errors[name]} </p>}
+     {/* let user {
+        name:  --> name is key 
+     } 
+     && if there is value 
+     
+     */}
 </div>
   )
 }
